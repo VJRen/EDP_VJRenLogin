614f1c438d29fc4a0310f1ba00a105b477038928
VJRen <laurenjamespaper@gmail.com>
2026-02-03 15:17:21 +0800

    this the first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TelanLaurenJamesBsit2D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbusername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbpassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tblogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(185, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // tbusername
+            // 
+            this.tbusername.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.tbusername.Location = new System.Drawing.Point(246, 164);
+            this.tbusername.Name = "tbusername";
+            this.tbusername.Size = new System.Drawing.Size(100, 20);
+            this.tbusername.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(187, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // tbpassword
+            // 
+            this.tbpassword.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.tbpassword.Location = new System.Drawing.Point(246, 198);
+            this.tbpassword.Name = "tbpassword";
+            this.tbpassword.PasswordChar = '*';
+            this.tbpassword.Size = new System.Drawing.Size(100, 20);
+            this.tbpassword.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(161, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(215, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Point of Sale System";
+            // 
+            // tblogin
+            // 
+            this.tblogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tblogin.BackgroundImage")));
+            this.tblogin.ForeColor = System.Drawing.Color.Maroon;
+            this.tblogin.Location = new System.Drawing.Point(232, 231);
+            this.tblogin.Name = "tblogin";
+            this.tblogin.Size = new System.Drawing.Size(75, 23);
+            this.tblogin.TabIndex = 5;
+            this.tblogin.Text = "Login";
+            this.tblogin.UseVisualStyleBackColor = true;
+            this.tblogin.Click += new System.EventHandler(this.tblogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(547, 351);
+            this.Controls.Add(this.tblogin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbpassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbusername);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbusername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbpassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button tblogin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelanLaurenJamesBsit2D
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tblogin_Click(object sender, EventArgs e)
+        {
+            if (tbusername.Text == "" || tbpassword.Text == "")
+            {
+                if (tbusername.Text == "")
+                {
+                    MessageBox.Show("Please enter Username");
+                    tbusername.Focus();
+                }
+                else if (tbpassword.Text == "")
+                {
+                    MessageBox.Show("Please enter Password");
+                    tbpassword.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Welcome "+ tbusername.Text);
+            }
+
+        }
+    }
+}
